cdb6fd88ae9904361f0944a748d214ba6a1e8a57
Oscar <>
2024-10-15 08:12:28 +0200

    add reviews animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import { Reviews } from "@/components/Reviews";
 import { Icons } from "@/components/Icons";
 import { Check, Star } from "lucide-react";
 
@@ -193,6 +194,10 @@ export default function Home() {
             </div>
           </div>
         </MaxWidthWrapper>
+
+        <div className="pt-16">
+          <Reviews />
+        </div>
       </section>
     </div>
   );
